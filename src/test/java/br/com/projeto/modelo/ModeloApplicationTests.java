00d5b196760cffe2773f75fb6dd1f69c542ee499
tliner-friaca-dev <tlinerfc@hotmail.com>
2022-02-09 17:01:52 -0300

    Commit

---
+++ src/test/java/br/com/projeto/modelo/ModeloApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.modelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
